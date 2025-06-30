fa60d1a4d36042191b2af8645061fb7e33e64754
Nick Raskop <raskop@usc.edu>
2025-06-30 00:52:43 -0700

    Add chess game board w/ checkered cells

--- chess.py
+++ chess.py
@@ -3,33 +3,66 @@ from pygame._sdl2 import Window
 
 # pygame setup
 pygame.init()
+pygame.display.set_caption("NICK CHESS")
 screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+gameBoard = pygame.Surface((50, 50))
 Window.from_display_module().maximize()
-clock = pygame.time.Clock()
+clock = (
+    pygame.time.Clock()
+)  # Not needed right now, but potentially will use later for time controls
 running = True
-dt = 0
 
-bgBrown = pygame.Color(168, 94, 50)
+BROWN = pygame.Color(168, 94, 50)
+
+
+def getColor(r, c):
+    color = "white"
+    if r % 2 == 0:
+        if c % 2 == 0:
+            color = "black"
+    else:
+        if c % 2 != 0:
+            color = "black"
+    return color
+
+
+def createGameBoard():
+    screenWidth, screenHeight = pygame.display.get_surface().get_size()
+    centerScreen = pygame.Vector2(screenWidth / 2, screenHeight / 2)
+    gameBoardSize = min(screenWidth, screenHeight) * 0.7
+    gameBoard = pygame.Surface((gameBoardSize, gameBoardSize))
+    gameBoard.fill("red")
+    gameBoardTopLeft = (
+        centerScreen.x - gameBoardSize / 2,
+        centerScreen.y - gameBoardSize / 2,
+    )
+
+    for row in range(8):
+        for col in range(8):
+            cell = pygame.Rect(
+                row * (gameBoardSize / 8),
+                col * (gameBoardSize / 8),
+                gameBoardSize / 8,
+                gameBoardSize / 8,
+            )
+            pygame.draw.rect(gameBoard, getColor(row, col), cell)
+
+    # Blit must come after everything in the gameBoard is updated
+    screen.blit(gameBoard, gameBoardTopLeft)
+
+
+def handleVideoResize():
+    screen.fill(BROWN)
+    createGameBoard()
+
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # Poll for events
     for event in pygame.event.get():
+        if event.type == pygame.VIDEORESIZE:
+            handleVideoResize()
         if event.type == pygame.QUIT:
             running = False
-
-    centerScreen = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-    w, h = pygame.display.get_surface().get_size()
-    gameBoardSize = min(w, h) * 0.7
-    gameBoard = pygame.Rect(0, 0, gameBoardSize, gameBoardSize)
-    gameBoard.center = centerScreen
-
-    screen.fill(bgBrown)
-
-    # Render checkerboard
-    # First, render outer square in the center of screen
-    pygame.draw.rect(screen, "red", gameBoard)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
